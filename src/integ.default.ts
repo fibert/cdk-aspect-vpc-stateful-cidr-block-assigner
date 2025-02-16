07a86e19a724ae26cc473fb096bcd46d8f49b2d7
Dor Fibert <dorfib@gmail.com>
2025-02-16 17:27:51 +0200

    refactor: IntegTesting class

--- src/integ.default.ts
+++ src/integ.default.ts
@@ -11,29 +11,24 @@ interface IntegTestingProps {
 export class IntegTesting {
   readonly stack: cdk.Stack;
   readonly app: cdk.App;
+  readonly vpc: ec2.Vpc;
 
   constructor(props: IntegTestingProps) {
 
     const ENV: cdk.Environment = { account: '027179758433', region: 'us-east-1' };
     this.app = new cdk.App();
-    this.stack = new cdk.Stack(this.app, 'demo', { env: ENV });
+    this.stack = new cdk.Stack(this.app, 'IntegrationTestStack', { env: ENV });
 
-    const vpc = new ec2.Vpc(this.stack, 'TestVpc', {
+    this.vpc = new ec2.Vpc(this.stack, 'Vpc', {
       ipAddresses: ec2.IpAddresses.cidr(props.cidrBlock),
       availabilityZones: props.availabilityZones,
       subnetConfiguration: props.subnetConfiguration,
     });
-
-    const vpcStatefulCidrBlockAssigner = new VpcStatefulCidrBlockAssigner({
-      vpcId: 'vpc-01b37083041d6f6c5',
-      // availabilityZoneSubstitutions: [{source: 'us-east-1b', target: 'us-east-1c'}],
-    });
-    cdk.Aspects.of(vpc).add(vpcStatefulCidrBlockAssigner, { priority: cdk.AspectPriority.MUTATING });
   }
 }
 
 const CIDR_BLOCK = '10.10.0.0/16';
-const AVAILABILITY_ZONES: Array<string> = ['us-east-1a', 'us-east-1c'];
+const AVAILABILITY_ZONES: Array<string> = ['us-east-1a', 'us-east-1b'];
 const SUBNET_CONFIGURATION: Array<ec2.SubnetConfiguration> = [
   { name: 'proxies', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC },
   { name: 'backend', cidrMask: 24, subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
@@ -45,4 +40,12 @@ const integ = new IntegTesting({
   availabilityZones: AVAILABILITY_ZONES,
   subnetConfiguration: SUBNET_CONFIGURATION,
 });
+
+
+const vpcStatefulCidrBlockAssigner = new VpcStatefulCidrBlockAssigner({
+  vpcId: 'vpc-01b37083041d6f6c5',
+  // availabilityZoneSubstitutions: [{source: 'us-east-1b', target: 'us-east-1c'}],
+});
+cdk.Aspects.of(integ.vpc).add(vpcStatefulCidrBlockAssigner, { priority: cdk.AspectPriority.MUTATING });
+
 integ.app.synth();
