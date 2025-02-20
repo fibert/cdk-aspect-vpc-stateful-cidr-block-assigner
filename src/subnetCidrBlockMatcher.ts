ec5874676606b83dc0d9bd63db5dbb5d12886986
Dor Fibert <dorfib@gmail.com>
2025-02-20 11:21:29 +0200

    docs: add documentation to SubnetMutator and SubnetCidrBlockMatcher

--- src/subnetCidrBlockMatcher.ts
+++ src/subnetCidrBlockMatcher.ts
@@ -1,12 +1,25 @@
 import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { SubnetMutator } from './subnetMutator';
 
+/**
+ * A class that manages the matching of ec2.Subnets with CIDR blocks.
+ * Maintains two queues - one for CIDR blocks waiting to be assigned to subnets,
+ * and another for subnets waiting to be assigned CIDR blocks.
+ */
 export class SubnetCidrBlockMatcher {
+  /** Queue of CIDR blocks waiting to be assigned to subnets */
   private freshCidrBlocksAwaitingSubnet: Array<string> = [];
+
+  /** Queue of subnets waiting to be assigned CIDR blocks */
   private subnetsAwaitingFreshCidrBlock: Array<ec2.Subnet> = [];
 
   constructor() {}
 
+  /**
+   * Attempts to match a subnet with an available CIDR block.
+   * If no CIDR block is available, adds the subnet to the waiting queue.
+   * @param subnet The subnet to be matched with a CIDR block
+   */
   matchSubnetWithCidrBlock(subnet: ec2.Subnet): void {
     const freshCidrBlock: string | undefined = this.freshCidrBlocksAwaitingSubnet.pop();
 
@@ -17,6 +30,11 @@ export class SubnetCidrBlockMatcher {
     }
   }
 
+  /**
+   * Attempts to match a CIDR block with a waiting subnet.
+   * If no subnet is waiting, adds the CIDR block to the waiting queue.
+   * @param cidrBlock The CIDR block to be matched with a subnet
+   */
   matchFreshCidrBlockWithSubnet(cidrBlock: string): void {
     const subnet: ec2.Subnet | undefined = this.subnetsAwaitingFreshCidrBlock.pop();
 
--- src/subnetMutator.ts
+++ src/subnetMutator.ts
@@ -1,6 +1,14 @@
 import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 
+/**
+ * Utility class for modifying ec2.Subnet configurations
+ */
 export class SubnetMutator {
+  /**
+   * Updates the CIDR block of an existing subnet
+   * @param subnet The EC2 subnet to modify
+   * @param newCidrBlock The new CIDR block to assign to the subnet
+   */
   public static setSubnetCidrBlock(subnet: ec2.Subnet, newCidrBlock: string): void {
     const l1Subnet = subnet.node.defaultChild as ec2.CfnSubnet;
     l1Subnet.addPropertyOverride('CidrBlock', newCidrBlock);
