51b48b9f3dc3d06b33fed58512b64e471f7f7dd1
Dor Fibert <dorfib@gmail.com>
2025-02-20 11:42:22 +0200

    refactor: use beforeAll and beforeEach

--- test/VpcStatefulCidrBlockAssigner/appendAvailabilityZones.test.ts
+++ test/VpcStatefulCidrBlockAssigner/appendAvailabilityZones.test.ts
@@ -25,34 +25,48 @@ class TestStack extends cdk.Stack {
   }
 }
 
-const APP = new cdk.App();
-const STACK = new TestStack(APP, 'IntegrationTestStack', {
-  env: constants.ENV,
-  cidrBlock: constants.VPC_CIDR_BLOCK,
-  availabilityZones: constants.AVAILABILITY_ZONES_A_B,
-  subnetConfiguration: constants.SUBNET_CONFIGURATION,
+let baseApp: cdk.App;
+let baseStack: TestStack;
+let baseTemplate: Template;
+let baseSubnets: { [key: string]: { [key: string]: any } };
+let baseSubnetRecords: Array<SubnetRecord>;
+
+beforeAll(() => {
+  baseApp = new cdk.App();
+  baseStack = new TestStack(baseApp, 'Stack', {
+    env: constants.ENV,
+    cidrBlock: constants.VPC_CIDR_BLOCK,
+    availabilityZones: constants.AVAILABILITY_ZONES_A_B,
+    subnetConfiguration: constants.SUBNET_CONFIGURATION,
+  });
+
+  baseTemplate = Template.fromStack(baseStack);
+  baseSubnets = baseTemplate.findResources('AWS::EC2::Subnet');
+  baseSubnetRecords = generateSubnetRecordsArray(baseSubnets);
 });
 
-const BASE_TEMPLATE = Template.fromStack(STACK);
-const BASE_SUBNETS = BASE_TEMPLATE.findResources('AWS::EC2::Subnet');
-const BASE_SUBNET_RECORDS = generateSubnetRecordsArray(BASE_SUBNETS);
+let testApp: cdk.App;
+let testStack: TestStack;
+
+beforeEach(() => {
+  testApp = new cdk.App();
+  testStack = new TestStack(testApp, 'Stack', {
+    env: constants.ENV,
+    cidrBlock: constants.VPC_CIDR_BLOCK,
+    availabilityZones: constants.AVAILABILITY_ZONES_A_B_C, // Add AZ C
+    subnetConfiguration: constants.SUBNET_CONFIGURATION,
+  });
+});
 
 describe('test appending availability zone', () => {
   test('without VpcStatefulCidrBlockAssigner expecting CIDR conflict', () => {
     // Given
 
     // When
-    const testApp = new cdk.App();
-    const testStack = new TestStack(testApp, 'addAzStack', {
-      env: constants.ENV,
-      cidrBlock: constants.VPC_CIDR_BLOCK,
-      availabilityZones: constants.AVAILABILITY_ZONES_A_B_C,
-      subnetConfiguration: constants.SUBNET_CONFIGURATION,
-    });
 
     // Then
     const testSubnetRecords = getSubnetRecordsFromStack(testStack);
-    const conflictingSubnetRecords = calculateConfilictingSubnets(BASE_SUBNET_RECORDS, testSubnetRecords);
+    const conflictingSubnetRecords = calculateConfilictingSubnets(baseSubnetRecords, testSubnetRecords);
 
     expect(conflictingSubnetRecords).not.toEqual([]);
   });
@@ -61,13 +75,6 @@ describe('test appending availability zone', () => {
     // Given
 
     // When
-    const testApp = new cdk.App();
-    const testStack = new TestStack(testApp, 'addAzStack', {
-      env: constants.ENV,
-      cidrBlock: constants.VPC_CIDR_BLOCK,
-      availabilityZones: constants.AVAILABILITY_ZONES_A_B_C,
-      subnetConfiguration: constants.SUBNET_CONFIGURATION,
-    });
     const vpcStatefulCidrBlockAssigner = new VpcStatefulCidrBlockAssigner({
       vpcId: constants.CONTEXT_FILE_VPC_ID,
       contextFileDirectory: constants.CONTEXT_FILE_DIRECTORY,
@@ -78,12 +85,11 @@ describe('test appending availability zone', () => {
 
     // Then
     const testSubnetRecords = getSubnetRecordsFromStack(testStack);
-    const conflictingSubnetRecords = calculateConfilictingSubnets(BASE_SUBNET_RECORDS, testSubnetRecords);
+    const conflictingSubnetRecords = calculateConfilictingSubnets(baseSubnetRecords, testSubnetRecords);
 
     expect(conflictingSubnetRecords).toStrictEqual([]);
   });
 });
-
 
 function getSubnetRecordsFromStack(stack: cdk.Stack): SubnetRecord[] {
   const template = Template.fromStack(stack);
